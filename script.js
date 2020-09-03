826c4e4e65bcce4688b38d910071de67ae9d866b
Aditya Pradhan <adityapradhan812@gmail.com>
2020-09-03 04:57:59 -0700

    ass

---
+++ script.js
@@ -0,0 +1,14 @@
+var startsWith;
+var a=["Alisha","marry","joseph","Kajol","Riya","John","Jamie","jully","Roshan","ROhan","Roshan"];
+for(i=0;i<a.length;i++)
+{
+if(a[i].startsWith('j')||a[i].startsWith('J'))
+{
+alert("GoodBye" +" " +"J" +a[i]+" ");
+}
+
+else
+{
+alert("Hello" +" "+a[i]+" ");
+}
+}
